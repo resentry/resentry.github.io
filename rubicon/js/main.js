5ea2435d68fe69cfda02be957087e782f3d440ae
resentry <80509313+resentry@users.noreply.github.com>
2021-05-20 10:58:15 +0300

    Update main.js

--- rubicon/js/main.js
+++ rubicon/js/main.js
@@ -16,20 +16,28 @@ $(document).ready(function () {
         }],
     });
 });
+
+let scrollLogo = document.getElementById('menuContainer');
+let menuItemIconMenu = document.createElement('li');
+menuItemIconMenu.id = 'menuItemIconMenu';
+
+
 document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener('scroll', function () {
         if (window.scrollY > 100) {
             document.getElementById('navbar_top').classList.add('fixed-top');
             document.getElementById('navbar_top').classList.add('shadow');
+            document.getElementById('menuContainer').prepend(menuItemIconMenu)
             // add padding top to show content behind navbar
-            navbar_height = document.querySelector('.navbar').offsetHeight;
-            document.body.style.paddingTop = navbar_height + 'px';
+            // navbar_height = document.querySelector('.navbar').offsetHeight;
+            // document.body.style.paddingTop = navbar_height + 'px';
         } else {
+            menuItemIconMenu.parentNode.removeChild(menuItemIconMenu);
             document.getElementById('navbar_top').classList.remove('fixed-top');
             document.getElementById('navbar_top').classList.remove('shadow');
+            // menuItemIconMenu.textContent = '';
+            // document.body.style.paddingTop = '0';
 
-            // remove padding top from body
-            document.body.style.paddingTop = '0';
         }
     });
 });
